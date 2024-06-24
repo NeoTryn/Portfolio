08b54dbea6331e4cdad6efcc411227faab99e5f1
NeoTryn <eidlerfelix@gmail.com>
2024-06-24 19:51:59 +0200

    lighting added, loading models not working

--- src/components/Views/GameView.vue
+++ src/components/Views/GameView.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import {
+  AmbientLight,
   BoxGeometry,
   Mesh,
-  MeshBasicMaterial,
-  PerspectiveCamera,
+  MeshBasicMaterial, MeshLambertMaterial, MeshPhongMaterial,
+  PerspectiveCamera, PointLight,
   Scene,
   WebGLRenderer
 } from 'three'
 import { onMounted, ref } from 'vue'
+import { GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 const canvas = ref<HTMLCanvasElement | null>(null)
 
@@ -18,6 +20,11 @@ scene.add(camera)
 
 camera.position.z = 5
 
+const loader = new GLTFLoader();
+
+const light = new PointLight(0xff0000, 100, 100);
+scene.add(light)
+
 onMounted(() => {
   const renderer = new WebGLRenderer({
     canvas: canvas.value as unknown as HTMLCanvasElement,
@@ -27,16 +34,36 @@ onMounted(() => {
   renderer.setSize(window.innerWidth, window.innerHeight)
 
   const geometry = new BoxGeometry(1, 1, 1)
-  const material = new MeshBasicMaterial({
-    color: getComputedStyle(document.documentElement).getPropertyValue('--brandcolor')
+  const material = new MeshPhongMaterial({
+    color: 0xffffff
   })
   const cube = new Mesh(geometry, material)
 
-  cube.rotation.z = Math.PI / 4
+  const light = new AmbientLight(0xffffff, 100);
+  scene.add(light)
 
   scene.add(cube)
 
-  renderer.render(scene, camera)
+  cube.rotation.z = Math.PI / 4
+
+  loader.load(
+    '/models/monkey.glb',
+    gltf => {
+      scene.add(gltf.scene)
+      console.log("added");
+    },
+    undefined,
+    undefined
+  )
+
+  renderer.render(scene, camera);
+
+  function render() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+  }
 })
 </script>
 
